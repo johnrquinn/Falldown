f7a8753018f89d99d1dc32c6200429fd36574773
johnRquinn <jrquin@gmail.com>
2017-08-20 23:41:05 -0400

    First commit, basic Falldown game mostly working. Needs sprites, menu, and fix on changing speed of line spawning based on score.

---
+++ WKFalldown Extension/GameScene.swift
@@ -0,0 +1,404 @@
+//
+//  GameScene.swift
+//  WKFalldown Extension
+//
+//  Created by John Quinn on 7/23/17.
+//  Copyright © 2017 Dilldrup LLC. All rights reserved.
+//
+
+import SpriteKit
+
+enum ColliderType: UInt32 {
+    
+    case Ball = 1
+    case Line = 2
+    case GameOver = 4
+    case Edge = 8
+    case Gap = 16
+    
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var score = 0
+    
+    var highScore = UserDefaults.standard.integer(forKey: "HIGHSCORE")
+    
+    let moveUp = SKAction.moveBy(x: 0, y:600, duration:5.0)
+    
+    var gameOver = false
+    
+    var ball = SKSpriteNode()
+    
+    let gameOverTest = SKSpriteNode()
+    
+    let rightEdge = SKSpriteNode()
+    
+    let leftEdge = SKSpriteNode()
+    
+    let bottomEdge = SKSpriteNode()
+    
+    var gapWidth = 50
+    
+    var halfGap = 0
+    
+    var scoreLabel = SKLabelNode()
+    
+    var gameOverLabel = SKLabelNode()
+    
+    var gameOverScoreLabel = SKLabelNode()
+    
+    var highScoreLabel = SKLabelNode()
+    
+    var wait = SKAction()
+    
+    var createForever = SKAction()
+    
+    override func sceneDidLoad() {
+        
+        physicsWorld.contactDelegate = self as SKPhysicsContactDelegate
+        
+        setUpGame()
+        
+    }
+    
+    func setUpGame() {
+        
+        // MAKE MENU THE FIRST VC THEN POP THE GAME ON TOP
+        
+        // TO DO: add scrolling background, update sprites for lines, add startup menu, fix spawn speed change when score goes up
+        
+        self.anchorPoint = CGPoint(x: 0, y: 0)
+        
+        score = 0
+        gapWidth = 55
+        
+        gameOver = false
+        
+        self.removeAllChildren()
+        self.removeAllActions()
+        
+        /* THIS CODE MANAGES THE BACKGROUND IN PGE, NEEDS TAILORING OBVIOUSLY
+ 
+         let bgTexture = SKTexture(imageNamed: "bg.png")
+ 
+         let moveBGAnimation = SKAction.move(by: CGVector(dx: -bgTexture.size().width, dy: 0), duration: 7)
+         let shiftBGAnimation = SKAction.move(by: CGVector(dx: bgTexture.size().width, dy: 0), duration: 0)
+         let moveBGForever = SKAction.repeatForever(SKAction.sequence([moveBGAnimation, shiftBGAnimation]))
+ 
+         var i: CGFloat = 0
+ 
+         while i < 3 {
+ 
+         bg = SKSpriteNode(texture: bgTexture)
+ 
+         bg.position = CGPoint(x: bgTexture.size().width * i, y: self.frame.midY)
+ 
+         bg.size.height = self.frame.height
+ 
+         bg.run(moveBGForever)
+ 
+         bg.zPosition = -2
+ 
+         self.addChild(bg)
+ 
+         i += 1
+ 
+         }
+ 
+        */
+ 
+        // SET UP THE BALL
+        
+        let ballTexture = SKTexture(imageNamed: "falldownBall.png")
+        ball = SKSpriteNode(texture: ballTexture)
+ 
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 20)
+        ball.physicsBody!.categoryBitMask = ColliderType.Ball.rawValue
+        ball.physicsBody!.collisionBitMask = ColliderType.Line.rawValue | ColliderType.Edge.rawValue
+        ball.physicsBody!.contactTestBitMask = ColliderType.GameOver.rawValue | ColliderType.Gap.rawValue | ColliderType.Line.rawValue
+        
+        ball.position = CGPoint(x: self.frame.midX, y: self.frame.midY + 150)
+        
+        self.addChild(ball)
+
+        // SET UP THE GAME OVER TRIGGER
+        
+        gameOverTest.alpha = 0
+        
+        gameOverTest.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: self.size.width, height: 5))
+        
+        gameOverTest.physicsBody!.isDynamic = false
+        
+        gameOverTest.physicsBody!.categoryBitMask = ColliderType.GameOver.rawValue
+        gameOverTest.physicsBody!.contactTestBitMask = ColliderType.Ball.rawValue | ColliderType.Line.rawValue
+        
+        gameOverTest.position = CGPoint(x: self.frame.midX, y: self.frame.maxY + 20)
+        
+        self.addChild(gameOverTest)
+        
+        // SET UP THE LEFT EDGE
+        
+        leftEdge.alpha = 0
+        
+        leftEdge.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 5, height: self.size.height))
+        
+        leftEdge.physicsBody!.isDynamic = false
+        
+        leftEdge.physicsBody!.categoryBitMask = ColliderType.Edge.rawValue
+        leftEdge.physicsBody!.collisionBitMask = ColliderType.Ball.rawValue
+        
+        leftEdge.position = CGPoint(x: self.frame.minX + 1, y: self.frame.midY)
+        
+        self.addChild(leftEdge)
+        
+        // SET UP THE RIGHT EDGE
+        
+        rightEdge.alpha = 0
+        
+        rightEdge.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 5, height: self.size.height))
+        
+        rightEdge.physicsBody!.isDynamic = false
+        rightEdge.physicsBody!.affectedByGravity = false
+        
+        rightEdge.physicsBody!.categoryBitMask = ColliderType.Edge.rawValue
+        rightEdge.physicsBody!.collisionBitMask = ColliderType.Ball.rawValue
+        
+        rightEdge.position = CGPoint(x: self.frame.maxX - 1, y: self.frame.midY)
+        
+        self.addChild(rightEdge)
+        
+        // SET UP THE BOTTOM EDGE
+        
+        bottomEdge.alpha = 0
+        
+        bottomEdge.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: self.size.width, height: 1))
+        
+        bottomEdge.physicsBody!.isDynamic = false
+        bottomEdge.physicsBody!.affectedByGravity = false
+        
+        bottomEdge.physicsBody!.categoryBitMask = ColliderType.Edge.rawValue
+        bottomEdge.physicsBody!.collisionBitMask = ColliderType.Ball.rawValue
+        
+        bottomEdge.position = CGPoint(x: self.frame.midX, y: self.frame.minY + 1)
+        
+        self.addChild(bottomEdge)
+        
+        // SET UP THE SCORE LABEL
+        
+        scoreLabel.position = CGPoint(x: self.frame.maxX - 50, y: self.frame.maxY - 50)
+        
+        scoreLabel.fontName = "Helvetica"
+        scoreLabel.fontSize = 30
+        scoreLabel.zPosition = 1
+        
+        scoreLabel.text = String(score)
+        
+        self.addChild(scoreLabel)
+        
+        // LOOP LINE CREATION 
+        
+        let screenWidth = self.frame.maxX
+        halfGap = gapWidth / 2
+        
+        createLine(at: CGPoint(x: CGFloat(arc4random_uniform(UInt32((Int(screenWidth) - halfGap) - halfGap)) + UInt32(halfGap)), y: self.frame.minY + 1), with: (CGFloat(self.gapWidth)))
+        
+        let create = SKAction.run { self.createLine(at: CGPoint(x: CGFloat(arc4random_uniform(UInt32((Int(screenWidth) - self.halfGap) - self.halfGap)) + UInt32(self.halfGap)), y: self.frame.minY + 1), with: (CGFloat(self.gapWidth))) }
+        
+        wait = SKAction.wait(forDuration: 0.8)
+    
+        createForever = SKAction.sequence([wait, create])
+        
+        self.run(SKAction.repeatForever(createForever))
+        
+    }
+    
+    func createLine(at location: CGPoint, with gapWidth: CGFloat) {
+        
+        let lineTexture = SKTexture(imageNamed: "horizontalLine.png")
+        let line2Texture = SKTexture(imageNamed: "horizontalLine2.png")
+        
+        let line = SKSpriteNode(texture: lineTexture)
+        let line2 = SKSpriteNode(texture: line2Texture)
+        
+        let gap = SKSpriteNode()
+    
+        line.physicsBody = SKPhysicsBody(rectangleOf: line.size)
+        line.physicsBody!.categoryBitMask = ColliderType.Line.rawValue
+        line.physicsBody!.contactTestBitMask = ColliderType.GameOver.rawValue
+        line.physicsBody!.collisionBitMask = ColliderType.Ball.rawValue
+        line.physicsBody!.isDynamic = false
+        
+        gap.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: gapWidth, height: (line.size.height / 4)))
+        gap.physicsBody!.categoryBitMask = ColliderType.Gap.rawValue
+        gap.physicsBody!.collisionBitMask = 0
+        gap.physicsBody!.contactTestBitMask = ColliderType.Ball.rawValue | ColliderType.GameOver.rawValue
+        gap.physicsBody!.isDynamic = false
+        
+        line2.physicsBody = SKPhysicsBody(rectangleOf: line2.size)
+        line2.physicsBody!.categoryBitMask = ColliderType.Line.rawValue
+        line2.physicsBody!.contactTestBitMask = ColliderType.GameOver.rawValue
+        line2.physicsBody!.collisionBitMask = ColliderType.Ball.rawValue
+        line2.physicsBody!.isDynamic = false
+        
+        gap.position = CGPoint(x: location.x, y: location.y - (line.size.height / 2))
+        self.addChild(gap)
+        
+        line.position = CGPoint(x: (location.x - (gapWidth / 2) - (line.size.width / 2)), y: location.y)
+        line2.position = CGPoint(x: (location.x + (gapWidth / 2) + (line2.size.width / 2)), y: location.y)
+        
+        self.addChild(line)
+        self.addChild(line2)
+        
+        line.run(moveUp)
+        gap.run(moveUp)
+        line2.run(moveUp)
+        
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        if (firstBody.categoryBitMask == 1 && secondBody.categoryBitMask == 4) {
+            // if ball hit GameOverTest
+            gameOver = true
+            
+            gameOverLabel.fontName = "Helvetica"
+            gameOverLabel.fontSize = 30
+            gameOverLabel.zPosition = 1
+
+            highScoreLabel.fontName = "Helvetica"
+            highScoreLabel.fontSize = 30
+            highScoreLabel.zPosition = 1
+            
+            gameOverScoreLabel.fontName = "Helvetica"
+            gameOverScoreLabel.fontSize = 30
+            gameOverScoreLabel.zPosition = 1
+            
+            gameOverLabel.text = "Game Over!"
+            gameOverScoreLabel.text = "You scored \(score)."
+            
+            gameOverLabel.position = CGPoint(x: self.frame.midX, y: self.frame.midY + 50)
+            gameOverScoreLabel.position = CGPoint(x: self.frame.midX, y: self.frame.midY + 15)
+            highScoreLabel.position = CGPoint(x: self.frame.midX, y: self.frame.midY - 20)
+            
+            if score > highScore {
+                
+                saveHighScore()
+                
+                highScoreLabel.text = "New high score is \(score)."
+                
+            } else {
+                
+                highScoreLabel.text = "High score is \(highScore)."
+                
+            }
+            
+            if gameOverLabel.parent == nil && highScoreLabel.parent == nil{
+                self.addChild(gameOverLabel)
+                self.addChild(gameOverScoreLabel)
+                self.addChild(highScoreLabel)
+            } // this is copied from PGE, was necessary for preventing crashes when the game ends. not sure if it's necessary here
+            
+        }
+
+        if (firstBody.categoryBitMask == 1 && secondBody.categoryBitMask == 16) {
+            // if ball hit gap
+            
+            score += 1
+            scoreLabel.text = String(score)
+            
+            // MAKE THE GAME HARDER AS SCORE INCREASES
+            
+            switch score {
+                
+            // CHANGE CASES BACK TO MAKE THE GAME LAST LONGER... NEED TO DETERMINE HOW LONG THE GAME should LAST
+                
+            // ALSO, KNOWN BUG, this doesn't work -- wait can change but createForever needs to be called at this point // TRYING TO GET LINES TO SPAWN EVENLY
+                
+            case 0...10:
+                gapWidth = 50
+                halfGap = gapWidth / 2
+                wait = SKAction.wait(forDuration: 0.8)
+            case 10...20:
+                gapWidth = 50
+                halfGap = gapWidth / 2
+                wait = SKAction.wait(forDuration: 0.8, withRange: 0.1)
+            case 21...30:
+                gapWidth = 45
+                halfGap = gapWidth / 2
+                wait = SKAction.wait(forDuration: 0.7, withRange: 0.1)
+            case 31...1000:
+                gapWidth = 42
+                halfGap = gapWidth / 2
+                wait = SKAction.wait(forDuration: 0.6, withRange: 0.1)
+            default:
+                gapWidth = 55
+                halfGap = gapWidth / 2
+                
+            }
+            
+            self.removeAllActions()
+            self.run(SKAction.repeatForever(createForever))
+            
+        }
+        
+            
+            if (firstBody.categoryBitMask == 2 && secondBody.categoryBitMask == 4 ) {
+                // if line hit game over test
+                contact.bodyA.node?.removeFromParent()
+            }
+            
+            if (firstBody.categoryBitMask == 4 && secondBody.categoryBitMask == 16 ) {
+                // if gap hit game over test
+                contact.bodyB.node?.removeFromParent()
+            }
+        
+    }
+    
+    func moveBallLeft() {
+        ball.isPaused = false
+        ball.physicsBody!.applyImpulse(CGVector(dx: -1, dy: 0))
+        
+    }
+    
+    func moveBallRight() {
+        ball.isPaused = false
+        ball.physicsBody!.applyImpulse(CGVector(dx: 1, dy: 0))
+    }
+    
+    func stopBall() {
+        ball.isPaused = true
+    }
+    
+    func restartGame() {
+        if gameOver == true {
+            self.removeAllActions()
+            self.removeAllChildren()
+            
+            setUpGame()
+        
+        }
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+
+
+func saveHighScore() {
+    UserDefaults.standard.set(score, forKey: "HIGHSCORE")
+}
+
+}
---
+++ WKFalldown Extension/InterfaceController.swift
@@ -0,0 +1,76 @@
+//
+//  InterfaceController.swift
+//  WKFalldown Extension
+//
+//  Created by John Quinn on 7/23/17.
+//  Copyright © 2017 Dilldrup LLC. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class InterfaceController: WKInterfaceController, WKCrownDelegate {
+    
+    private var game: GameScene!
+
+    @IBOutlet var skInterface: WKInterfaceSKScene!
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        crownSequencer.delegate = self
+        crownSequencer.focus()
+        
+        // Configure interface objects here.
+        
+        // Load the SKScene from 'GameScene.sks'
+        if let scene = GameScene(fileNamed: "GameScene") {
+            
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            game = scene
+            
+            // Present the scene
+            self.skInterface.presentScene(scene)
+            
+            crownSequencer.delegate = self
+            crownSequencer.focus()
+            
+            // Use a value that will maintain a consistent frame rate
+            self.skInterface.preferredFramesPerSecond = 30
+            
+        }
+    }
+    
+    func crownDidRotate(_ crownSequencer: WKCrownSequencer?, rotationalDelta: Double) {
+        
+        if rotationalDelta > 0 {
+            game.moveBallRight()
+            
+        } else if rotationalDelta < 0 {
+            game.moveBallLeft()
+        }
+    }
+    
+    func crownDidBecomeIdle(_ crownSequencer: WKCrownSequencer?) {
+        game.stopBall()
+    }
+    
+    @IBAction func handleSingleTap(_ sender: Any) {
+        
+        game.restartGame()
+    
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
